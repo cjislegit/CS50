cb3329e068f3eecc7624596a9eb8991b2d14ddc2
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-03-03 21:18:39 -0800

    Updated code styling

--- mario1.c
+++ mario1.c
@@ -7,22 +7,22 @@ int main(void)
 
  do
   {
-    h = get_int("Enter a number between 1 and 8\n");
-    printf("Number: %i\n", h);
-  }while(h<1 || h>8);
+        h = get_int("Enter a number between 1 and 8\n");
+    }
+    while (h < 1 || h > 8);
 
-  for(int i = 0; i < h; i++)
-   {
-      for(int j = h-1; j > i; j--)
-       {
-        printf(" ");
-       }
-       for(int t = 0; t <= i; t++)
-       {
-           printf("#");
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = h - 1; j > i; j--)
+        {
+            printf(" ");
         }
-       printf("\n");
-   };
+        for (int t = 0; t <= i; t++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    };
   
 
 };
