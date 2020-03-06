b58f9ca15d484fe778697c765493632fada4e9ac
unknown <carlosjramirez11@gmail.com>
2020-03-05 16:27:39 -0800

    Started working on cash project

---
+++ cash.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+float c;
+void ask_for_Change(void);
+int round_change(void);
+
+int main(void)
+{
+    ask_for_Change();
+    int coins = round_change();
+}
+
+void ask_for_Change(void)
+{
+    do
+    {
+        c = get_float("Change owed: ");
+    }
+    while (c <= 0);
+    
+}
+
+int round_change(void)
+{
+    return round(c * 100);
+}
