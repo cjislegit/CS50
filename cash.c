53c210a88898d29067c7d35d33d04ff3860217c4
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-03-10 20:54:58 -0700

    Added solution for cash problem

--- cash.c
+++ cash.c
@@ -8,9 +8,24 @@ int round_change(void);
 
 int main(void)
 {
-    int num_coins = 0;
     ask_for_Change();
-    int coins = round_change();
+    int change = round_change();
+    int coins = 0;
+    int coin_values[] = {25, 10, 5, 1};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (change >= coin_values[i])
+        {
+            do
+            {
+                change -= coin_values[i];
+                coins++;
+            } while (change >= coin_values[i]);
+        }
+    }
+
+    printf("%i", coins);
 }
 
 void ask_for_Change(void)
@@ -18,9 +33,7 @@ void ask_for_Change(void)
     do
     {
         c = get_float("Change owed: ");
-    }
-    while (c <= 0);
-    
+    } while (c <= 0);
 }
 
 int round_change(void)
